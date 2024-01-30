751948be6cfdfae3591983f5fae3d5659bd10f2d
eymerin <garrett.bryce.young@gmail.com>
2024-01-30 00:09:00 -0700

    got navbar to display

--- portfolio/src/components/navbar/navbar.js
+++ portfolio/src/components/navbar/navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
-import contactImg from '../../assets/contactImg.jpg';
+import contactImg from '../../assets/contactImg.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={logo} alt="logo" className="logo">logo</img>
+            <img src={logo} alt="logo" className="logo"/>
             <div className="desktopMenu">
                 <Link className="desktopMenuListItem">Home</Link>
                 <Link className="desktopMenuListItem">About</Link>
@@ -15,7 +15,8 @@ const Navbar = () => {
                 <Link className="desktopMenuListItem">Contact</Link>
             </div>
             <button className="desktopMenuBtn">
-                <img src={contactImg} alt="contact" className="desktopMenuImg">Contact Me</img>
+                <img src={contactImg} alt="contact" className="desktopMenuImg"/>
+                Contact Me
             </button>
         </nav>
     )
