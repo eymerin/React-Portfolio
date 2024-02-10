bb317a1a9c589a391d7cb2554fccfc724603383e
eymerin <garrett.bryce.young@gmail.com>
2024-02-10 01:07:46 -0700

    finished resume downloader and adjusted sizing

--- portfolio/src/components/skills/skills.js
+++ portfolio/src/components/skills/skills.js
@@ -12,11 +12,11 @@ const Skills = () => {
             After getting my Bachelor's Degree in Game Design I added to it a Full Stack Development Certificate. 
             My passion is designing beautiful, responsive, and engaging user experiences.
             </span>
-            {/* Resume Download Link */}
+            <br/>
             <div className='resumeDownload'>
-                <a href="../../../public/files/Garrett_Young_Resume.docx" download="YourName_Resume.pdf" className="downloadResumeButton">
-                    Download My Resume
-                </a>
+            <a href={`${process.env.PUBLIC_URL}/files/Garrett_Young_Resume.pdf`} download="Garrett_Young_Resume.pdf" className="downloadResume">
+                - Download My Resume -
+            </a>
             </div>
             <div className='skillBars'>
                 <div className='skillBar'>
